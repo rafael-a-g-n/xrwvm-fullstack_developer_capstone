cdd98fda6df51871c353f0a6eb896ed699eba23f
rafael-a-g-n <ragn.ryzen@gmail.com>
2026-03-16 18:23:50 +0000

    Register CarMake and CarModel models in admin, and implement model definitions

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,8 +1,8 @@
 # Uncomment the following imports before adding the Model code
 
-# from django.db import models
-# from django.utils.timezone import now
-# from django.core.validators import MaxValueValidator, MinValueValidator
+from django.db import models
+from django.utils.timezone import now
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 
 # Create your models here.
@@ -12,7 +12,16 @@
 # - Description
 # - Any other fields you would like to include in car make model
 # - __str__ method to print a car make object
+class CarMake(models.Model):
+    """Model representing a car manufacturer."""
 
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    country = models.CharField(max_length=100, blank=True)
+    website = models.URLField(blank=True)
+
+    def __str__(self):
+        return self.name
 
 # <HINT> Create a Car Model model `class CarModel(models.Model):`:
 # - Many-To-One relationship to Car Make model (One Car Make has many
@@ -23,3 +32,29 @@
 # - Year (IntegerField) with min value 2015 and max value 2023
 # - Any other fields you would like to include in car model
 # - __str__ method to print a car make object
+
+class CarModel(models.Model):
+    """Model representing a car model made by a CarMake."""
+
+    car_make = models.ForeignKey(
+        CarMake,
+        on_delete=models.CASCADE,
+    )
+    dealer_id = models.IntegerField()
+    name = models.CharField(max_length=100)
+    CAR_TYPES = [
+        ("SEDAN", "Sedan"),
+        ("SUV", "SUV"),
+        ("WAGON", "Wagon"),
+    ]
+    type = models.CharField(max_length=10, choices=CAR_TYPES, default="SUV")
+    year = models.IntegerField(
+        default=2023,
+        validators=[
+            MaxValueValidator(2023),
+            MinValueValidator(2015),
+        ],
+    )
+
+    def __str__(self):
+        return f"{self.car_make.name} {self.name}"
